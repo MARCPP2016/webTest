acedff03c1ebc6cc6ba3f700ba002552c043d423
marcelo <marartb4@hotmail.com>
2018-06-23 23:24:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<meta charset="utf-8">
+ <script type="text/javascript" 
+ src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"> 
+ </script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<link rel="stylesheet" type="text/css" href="1.css">
+<head>
+  
+	<title></title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li id="Legacy" class="nav-item">
+        <a  class="nav-link" href="Legacy.html">Legacy</a>
+      </li>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Content
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="Movies.html">Movie Trailers</a>
+          <a class="dropdown-item" href="#">Expended Universe</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<audio id="myaudio" src="sound/1.mp3" autoplay="loop" ></audio>
+<img src="img/1.jpg" height="800" width="100%">
+
+
+<p id="primera">
+	Star Wars: The Force Awakens (also known as Star Wars: Episode VII – The Force Awakens) is a 2015 American epic space opera film produced, co-written and directed by J. J. Abrams. It is the first installment of the Star Wars sequel trilogy and the seventh main installment of the Star Wars franchise, following Return of the Jedi (1983). The film stars Harrison Ford, Mark Hamill, Carrie Fisher, Adam Driver, Daisy Ridley, John Boyega, Oscar Isaac, Lupita Nyong'o, Andy Serkis, Domhnall Gleeson, Anthony Daniels, Peter Mayhew, and Max von Sydow, and was produced by Lucasfilm Ltd. and Abrams' production company Bad Robot Productions. The Force Awakens is the first Star Wars film to not involve franchise creator George Lucas. Set 30 years after Return of the Jedi, the film follows Rey, Finn, and Poe Dameron's search for Luke Skywalker and their fight alongside the Resistance, led by veterans of the Rebel Alliance, against Kylo Ren and the First Order, a successor to the Galactic Empire.
+</p>
+<p>
+   The Force Awakens was announced after The Walt Disney Company's acquisition of Lucasfilm in October 2012. The film was produced by Abrams, his longtime collaborator Bryan Burk, and Lucasfilm president Kathleen Kennedy. Abrams and Lawrence Kasdan, co-writer of the original trilogy films The Empire Strikes Back (1980) and Return of the Jedi (1983), rewrote an initial script by Michael Arndt. John Williams, the composer for the previous six films, returned to compose the film's score. Lucas served as creative consultant during the film's early production.
+  Filming began in April 2014 in Abu Dhabi and Iceland, with principal photography also taking place in Ireland and Pinewood Studios in the United Kingdom, and concluded in November 2014. It is the first live-action Star Wars film since Star Wars: Episode III – Revenge of the Sith (2005).
+</p>
+<p>
+Star Wars: The Force Awakens was widely anticipated, and Disney backed the film with extensive marketing campaigns. It premiered in Los Angeles on December 14, 2015 before its wide release in the United States on December 18. The film received positive reviews from critics, with praise aimed at its ensemble cast, Abrams' direction, musical score, visual effects, and action sequences, although it received criticism for being derivative of the original trilogy. The film broke various box office records and became, unadjusted for inflation, the highest-grossing installment in the franchise, the highest-grossing film in North America, the highest-grossing film of 2015, and the third-highest-grossing film of all time, with a worldwide gross of over $2 billion and a net profit of over $780 million.It received five Academy Award nominations and four British Academy Film Award nominations, where it won the award for Best Special Visual Effects. It was followed by The Last Jedi in December 2017, with a third film scheduled for release in 2019.
+</p>
+
+	
+<h1>Plot</h1>
+<p>
+Thirty years after the Galactic Civil War, the First Order has risen from the fallen Galactic Empire and seeks to eliminate the New Republic. The Resistance, backed by the Republic and led by General Leia Organa, opposes them while Leia searches for her brother, Luke Skywalker.
+
+On the planet Jakku, Resistance pilot Poe Dameron receives a map to Luke's location from village elder Lor San Tekka. Stormtroopers commanded by Kylo Ren destroy the village and capture Poe, while Kylo kills San Tekka. Poe's droid BB-8 escapes with the map and encounters a scavenger, Rey, near a junkyard settlement. Kylo tortures Poe using the Force and learns of BB-8. Stormtrooper FN-2187, unwilling to kill for the First Order, frees Poe, and they escape in a stolen TIE fighter; Poe names FN-2187 "Finn". They crash on Jakku; Finn survives and assumes Poe died in the crash. Finn encounters Rey and BB-8, but the First Order tracks them and launches an airstrike. Finn, Rey, and BB-8 flee the planet in the Millennium Falcon, which they steal from a junkyard.
+</p>
+<p>
+The Falcon is captured by a larger ship piloted by Han Solo and Chewbacca, looking to reclaim their former vessel. They are attacked by gangs seeking to settle debts with Han, and flee in the Falcon. The gangs inform the First Order. At the First Order's Starkiller Base, a planet converted into a superweapon that harnesses star energy, Supreme Leader Snoke allows General Hux to use the weapon for the first time. Snoke questions Kylo's ability to deal with emotions relating to his father, Han Solo; Kylo says Han means nothing to him.
+</p>
+<p>
+<img src="img/2.jpg" height="600" width="100%" style="border-color: white">
+
+The Falcon crew determines BB-8's map is incomplete. Han explains that Luke attempted to rebuild the Jedi Order but exiled himself when an apprentice turned to the dark side, destroyed the temple, and slaughtered the other apprentices. The crew travels to the planet Takodana and meets with cantina owner Maz Kanata, who offers assistance getting BB-8 to the Resistance. Rey is drawn to a secluded vault and finds the lightsaber that once belonged to Luke and his father, Anakin Skywalker. She experiences disturbing visions and flees into the woods. Maz gives Finn the lightsaber for safekeeping.
+</p>
+
+<p>
+Starkiller Base fires and destroys the Republic capital star system and a portion of its fleet. The First Order attacks Takodana in search of BB-8. Han, Chewbacca, and Finn are saved by Resistance X-wing fighters led by Poe, who survived the crash on Jakku. Leia arrives at Takodana with C-3PO and reunites with Han and Chewbacca. Meanwhile, Kylo captures Rey and takes her to Starkiller Base, but she resists his mind-reading attempts. Discovering she can use the Force, Rey escapes by using a Jedi mind trick on a nearby guard.
+</p>
+
+<p>
+At the Resistance base on D'Qar, BB-8 finds R2-D2, who has been inactive since Luke's disappearance. As Starkiller Base prepares to fire on D'Qar, the Resistance devises a plan to destroy it by attacking a critical facility. Using the Falcon, Han, Chewbacca, and Finn infiltrate the facility, find Rey, and plant explosives. Han confronts Kylo, calling him by his birth name, Ben, and implores him to abandon the dark side. Kylo refuses and kills Han, enraging Chewbacca, who shoots Kylo and sets off the explosives, allowing the Resistance to attack and destroy Starkiller Base.
+</p>
+<p>
+The injured Kylo pursues Finn and Rey to the surface. Kylo defeats Finn in a lightsaber duel and leaves him badly wounded. Rey takes the lightsaber and uses the Force to defeat Kylo, before they are separated by a fissure as the planet begins to disintegrate. Snoke orders Hux to evacuate and bring Kylo to him to complete his training. Rey and Chewbacca escape with the unconscious Finn in the Falcon. On D'Qar, the Resistance celebrates while Leia, Chewbacca, and Rey mourn Han's death. R2-D2 awakens and reveals the rest of the map, which Rey follows to the oceanic planet Ahch-To. She finds Luke and presents him with the lightsaber.
+</p>
+
+<h2>Critical response</h2>
+
+<p>
+Star Wars: The Force Awakens received overwhelmingly positive reviews from critics. On review aggregator website Rotten Tomatoes, it has a 93% approval rating based on 383 reviews, with an average rating of 8.2/10. The website's critical consensus reads, "Packed with action and populated by both familiar faces and fresh blood,<img  
+  id="r" src="img/4.jpg" height="500" vspace="10" hspace="10" width="300" align="left"> The Force Awakens successfully recalls the series' former glory while injecting it with renewed energy."[366] On Metacritic, the film has a score of 81 out of 100, based on 54 reviews, indicating "universal acclaim".[367] Audiences polled by CinemaScore gave the film an average grade of "A" on an A+ to F scale; women, under-25s and under-18s gave it an "A+", while 98% of audiences gave it either an "A" or a "B".
+</p>
+<p>
+Robbie Collin of The Daily Telegraph gave The Force Awakens five stars out of five and wrote that it "sets out to shake Star Wars from its slumber, and reconnect the series with its much-pined-for past. That it achieves this both immediately and joyously is perhaps the single greatest relief of the movie-going year." Peter Bradshaw of The Guardian gave it five stars out of five, writing that it was "both a narrative progression from the earlier three films and a shrewdly affectionate next-gen reboot … ridiculous and melodramatic and sentimental of course, but exciting and brimming with energy and its own kind of generosity." Variety's Justin Chang wrote that the film has "sufficient style, momentum, love and care to prove irresistible to any who have ever considered themselves fans".Richard Roeper of the Chicago Sun-Times gave the film four stars out of four, describing it as "a beautiful, thrilling, joyous, surprising, and heart-thumping adventure".Tom Long of The Detroit News wrote that though some may find the film too similar to the original Star Wars, it leaves "the ungainly and unneeded clumsiness of the subsequent prequels far behind … the energy, humor and simplicity of direction [has] been recaptured". The Associated Press called it "basically the same" as the original film but "isn't that what we all wanted anyway?"
+</p>
+
+<p>
+Ann Hornaday, writing for The Washington Post, thought the film had "enough novelty to create yet another cohort of die-hard fans … The Force Awakens strikes all the right chords, emotional and narrative, to feel both familiar and exhilaratingly new."Christopher Orr, writing in The Atlantic, called the film "a mashup masterpiece" that "may be completely derivative" but is "a delight nonetheless". The Charlotte Observer's Lawrence Toppman said Abrams had "pulled off a delicate balancing act, paying clever homage to the past."[377] Mick LaSalle of the San Francisco Chronicle gave the film his highest rating and called it "the best Star Wars sequel yet and one of the best films of 2015".[378] Frank Pallotta, reviewing the film for CNN Money, found it was the best Star Wars film since the original trilogy and that it "is bound to be a film experience long remembered by fans and non-fans alike".[379] Jack Bottomley, writing for Starburst, awarded the film a 10 out of 10 score, saying "From old joys to fresh twists, this is a unique blockbuster experience that celebrates the franchise whilst also expanding it."
+
+</p>
+<p>
+ 
+ Some critics found The Force Awakens was too derivative of the original Star Wars trilogy,expressing in particular that the film is essentially a remake of A New Hope.Andrew O'Hehir wrote for Salon that it was "the work of a talented mimic or ventriloquist who can just about cover for the fact that he has nothing much to say".Stephanie Zacharek of TIME wrote that Abrams had delivered "everything we expect, as opposed to those nebulous wonders we didn't know we wanted".[384] Reviewing for Forbes, Scott Mendelson cited the film's "top-tier production values and a strong sense of scale and scope", but felt it was so much "an exercise in fan service [that] it is only due to the charisma and talent of our newbies and J. J. Abrams' undeniable skill as a visual storyteller that the Mad Libs narrative doesn't outright destroy the picture".Brian Merchant of Motherboard wrote that "Science fiction is supposed to be all about exploring the unexplored, not rehashing the well-trod … one of the most unabashedly creative enterprises of the 20th century has been rendered another largely enjoyable, but mostly forgettable Hollywood reboot.
+</p>
+
+</script>
+<script>
+  var audio = document.getElementById("myaudio");
+  audio.volume = 0.1;
+</script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<img    id="end"  src="img/7.jpg" height="300" width="100%" >
+</body>
+</html>
